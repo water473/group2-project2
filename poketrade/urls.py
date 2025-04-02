c6edf5771fb26f68f158e1ecd8fc2ee198f741d9
water473 <dimitriosrko114@gmail.com>
2025-04-01 22:39:36 -0400

    Implemented about page - could turn this into our help / tutorial page

--- poketrade/urls.py
+++ poketrade/urls.py
@@ -19,10 +19,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
     path('accounts/', include('accounts.urls')),
     path('pokemon/', include('pokemon.urls')),
     path('trading/', include('trading.urls')),
