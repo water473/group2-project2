68f078ede831c22a6baa5c16beebfc4d9eb4f51f
water473 <dimitriosrko114@gmail.com>
2025-04-19 22:18:34 -0400

    Updated stat's section to show trades completed

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.db.models import Q
 
 # Create your models here.
 
@@ -20,13 +21,20 @@ class Profile(models.Model):
     @property
     def trade_count(self):
         """Return the number of completed trades by the user."""
-        # This will need to be updated when the Trade model is implemented
-        return 0
+        from trading.models import TradeOffer
+        return TradeOffer.objects.filter(
+            Q(sender=self.user) | Q(recipient=self.user),
+            status__in=['accepted', 'completed']
+        ).count()
     
     @property
     def market_listings(self):
         """Return the number of active market listings by the user."""
-        return self.user.pokemon.filter(is_for_sale=True).count()
+        from marketplace.models import MarketplaceListing
+        return MarketplaceListing.objects.filter(
+            seller=self.user,
+            status='active'
+        ).count()
     
     def __str__(self):
         return self.user.username
